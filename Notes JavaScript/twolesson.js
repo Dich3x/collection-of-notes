9c7b0905c284d781de8b8a8d9e2410627bb23dab
Dich3x <aleksprox9@gmail.com>
2024-09-15 21:53:37 +0300

    Save 7

--- Notes JavaScript/twolesson.js
+++ Notes JavaScript/twolesson.js
@@ -1,7 +1,10 @@
-try {
-    const a = 5;
-} finally {
-    console.log("finally")
+function a2b2(num1, num2) {
+    try {
+        if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+            throw error = new Error("Оба значения должны быть числами")
+        } else {console.log(`${num1}^2 + ${num2}^2 = ${num1**2 + num2**2}`)}
+    }catch (e) {
+        console.log("Error: " + e.message)
+    }
 }
-const a = 8;
-console.log(a)
+a2b2(65, "сторока") 

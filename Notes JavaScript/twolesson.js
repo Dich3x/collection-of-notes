8f5a631091d8056066f43cfecda0b6c8102996b7
Dich3x <aleksprox9@gmail.com>
2024-09-15 17:23:53 +0300

    Save 6
    Дополнил tray...catch

--- Notes JavaScript/twolesson.js
+++ Notes JavaScript/twolesson.js
@@ -1,23 +1,7 @@
-function stringCheck(string1, string2) {
-    try {
-        if (typeof(string1) === "string" && typeof(string2) === "string") {
-            console.log(string1 + " " + string2);
-        } else throw new Error('Один из элементов не строка')
-    } catch (err) {
-        console.log(err.message + " - нужно передать строки")
-    } finally {
-        console.log("Функция завершила работу")
-    }
+try {
+    const a = 5;
+} finally {
+    console.log("finally")
 }
-
-stringCheck("strin1", 3)
-
-// try {
-//     if(1 === 2) {
-//         console.log('s')
-//     } else {
-//         throw new Error('sss')
-//     }
-// } catch(e) {
-//     console.log(e.message)
-// };
+const a = 8;
+console.log(a)
